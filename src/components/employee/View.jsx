5e73137e204de7b718846f457d6ba013128ce2fc
Urvi-patel27 <urvikathrotiyal@gmail.com>
2025-08-29 09:42:48 +0530

    n

--- src/components/employee/View.jsx
+++ src/components/employee/View.jsx
@@ -41,29 +41,29 @@ function View() {
           <div className='grid grid-cols-1 gap-6 w-full'>
 
             <div className="flex flex-col md:flex-row justify-center items-center md:space-x-4 mb-4">
-              <p className="text-lg font-semibold text-gray-700 w-40 text-center">Name:</p>
+              <p className="text-lg font-semibold text-gray-700 w-40">Name:</p>
               <p className="text-lg text-gray-900">{employee.userId.name}</p>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center md:space-x-4 mb-4">
-              <p className="text-lg font-semibold text-gray-700 w-40 text-center">Employee ID:</p>
+              <p className="text-lg font-semibold text-gray-700 w-40">Employee ID:</p>
               <p className="text-lg text-gray-900">{employee.employeeId}</p>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center md:space-x-4 mb-4">
-              <p className="text-lg font-semibold text-gray-700 w-40 text-center">Date of Birth:</p>
+              <p className="text-lg font-semibold text-gray-700 w-40">Date of Birth:</p>
               <p className="text-lg text-gray-900">
                 {new Date(employee.dob).toLocaleDateString()}
               </p>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center md:space-x-4 mb-4">
-              <p className="text-lg font-semibold text-gray-700 w-40 text-center">Gender:</p>
+              <p className="text-lg font-semibold text-gray-700 w-40">Gender:</p>
               <p className="text-lg text-gray-900">{employee.gender}</p>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center md:space-x-4 mb-4">
-              <p className="text-lg font-semibold text-gray-700 w-40 text-center">Department:</p>
+              <p className="text-lg font-semibold text-gray-700 w-40">Department:</p>
               <p className="text-lg text-gray-900">{employee.department.dep_name}</p>
             </div>
             <div className="flex flex-col md:flex-row justify-center items-center md:space-x-4 mb-4">
-              <p className="text-lg font-semibold text-gray-700 w-40 text-center">Marital Status:</p>
+              <p className="text-lg font-semibold text-gray-700 w-40">Marital Status:</p>
               <p className="text-lg text-gray-900">{employee.maritalStatus}</p>
             </div>
           </div>
